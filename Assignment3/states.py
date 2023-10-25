72af0a7b46c9dfa3d6bbe2031e6c983735954b09
davidoises <davidoises@hotmail.com>
2023-10-24 19:20:49 -0700

    Minor restructuring before working on different drives
    Added differential drive state class and working valet example
    
    Initial minor modifications for ackerman trailer drive
    
    Addition of sprite for trailer control
    
    Added base class for ackermann trailer
    
    initial tests with ackermann trailer drive
    
    Finalized trailer version
    
    Added python requirements file

--- Assignment3/states.py
+++ Assignment3/states.py
@@ -153,8 +153,266 @@ class AckermannDriveState(State):
         return f"x: {self.x} y: {self.y} angle: {degrees(self.angle)} psi: {degrees(self.psi)} v: {self.v} cost: {self.cost}"
 
 
+class DifferemtialDriveState(State):
 
+    # Max wheel speed of half a revolution per second
+    MAX_U_RAD_PER_S = pi
 
+    # 15cm wheel radius
+    WHEEL_RADIUS_M = 0.15
 
+    WHEEL_DISTANCE_M = 0.5
 
+    WHEEL_DISTANCE_PIXELS = 52
 
+    def __init__(self, x, y, angle, ul, ur, dt):
+        super().__init__(x, y, angle)
+        self.ul = ul
+        self.ur = ur
+        self.dt = dt
+
+    def get_steps(self, dt, ul, ur, current_angle):
+        """ This function returns the steps after integrating the kinematic model over dt """
+        omega = (-ul+ur)*self.WHEEL_RADIUS_M/self.WHEEL_DISTANCE_M
+        dangle = omega*dt
+        angle = (current_angle + dangle)%(2*pi)
+
+        v = (ul + ur)*self.WHEEL_RADIUS_M/2.0
+        v = v * self.WHEEL_DISTANCE_PIXELS/self.WHEEL_DISTANCE_M
+        dx = v*cos(angle)*dt
+        dy = -v*sin(angle)*dt # invert y due to pixel orientation
+
+        return dangle, dx, dy
+
+    def get_neighbors(self, vehicle, obstacles, dt_horizon, open_states, closed_states, goal_state, xlimit, ylimit):
+        """ This function expands the reachable states from the current state given the kinematic constraints
+            and calculates the cost to reach the new states using the path cost and a heuristic cost
+        """
+        neighbors = []
+
+        motions = [radians(180), radians(90), radians(45), radians(30), radians(0), radians(-30), radians(-45), radians(-90), radians(-180)]
+        for ul in motions:
+            for ur in motions:
+                dangle, dx, dy = self.get_steps(dt_horizon, ul, ur, self.angle)
+
+                angle = (self.angle + dangle)%(2*pi)
+
+                x = round(self.x + dx)
+                y = round(self.y + dy)
+
+                original_x = vehicle.x
+                original_y = vehicle.y
+                original_angle = vehicle.orientation
+
+                # 1. Check if its withing map limits
+                if x >= xlimit[0] and x <= xlimit[1] and y >= ylimit[0] and y <= ylimit[1]:
+
+                    # 2. Check if the state hasnt been explored yet
+                    new_state = DifferemtialDriveState(x, y, angle, ul, ur, dt_horizon)
+                    if new_state not in closed_states:
+
+                        # 3. Check if the new state is in collision
+                        vehicle.set_position(x, y)
+                        vehicle.set_orientation(degrees(angle))
+                        vehicle.update()
+                        colided = vehicle.check_collision(obstacles)
+                        if not colided:
+
+                            # Calculate the new costs
+                            tentative_new_cost = self.cost + self.calculate_euclidean_distance(new_state)
+                            heuristic_cost = new_state.calculate_heuristic_cost(self, goal_state)
+
+                            # Update the state in the open list if it exists
+                            try:
+                                index = open_states.index(new_state)
+                                previous_cost = open_states[index].cost
+                                if tentative_new_cost < previous_cost:
+                                    open_states[index] = new_state
+                                    open_states[index].cost = tentative_new_cost
+                                    open_states[index].total_cost = tentative_new_cost + heuristic_cost
+                                    open_states[index].parent_state = self
+                            except ValueError:
+                                # return a new element in the neighbors list if this hadnt been discovered
+                                new_state.parent_state = self
+                                new_state.cost = tentative_new_cost
+                                new_state.total_cost = tentative_new_cost + heuristic_cost
+                                neighbors.append(new_state)
+                        vehicle.set_position(original_x, original_y)
+                        vehicle.set_orientation(original_angle)
+                        vehicle.update()
+
+        return neighbors
+
+    def calculate_heuristic_cost(self, prev_state, goal_state):
+        """ Heuristic cost calculation penalizing:
+        euclidean distance from goal
+        driving in reverse
+        agressive changes of steering
+        the difference between the current orientation of the vehicle and the goal orientation
+        """
+
+        distance_multiplier = 2
+        reverse_multiplier = 1
+        if (self.ul+self.ur) < 0:
+            reverse_multiplier = 1.8
+        dist = self.calculate_euclidean_distance(goal_state)
+        angle_diff = degrees(abs(prev_state.angle-goal_state.angle)%(2*pi))
+
+        return reverse_multiplier*distance_multiplier*dist + 0.5*angle_diff
+
+    def goal_check(self, other_state):
+        """ Checks if the current state is whithin acceptable tolerance from the goal """
+        dist = self.calculate_euclidean_distance(other_state)
+        angle_diff = degrees(abs(self.angle - other_state.angle)%(2*pi))
+
+        return dist <30 and angle_diff < 5
+
+    def __eq__(self, other_state):
+        """ Compares 2 states considering certain tolerance """
+        dist = self.calculate_euclidean_distance(other_state)
+        angle_diff = degrees(abs(self.angle - other_state.angle)%(2*pi))
+
+        return dist < 30 and angle_diff < 5
+
+    def __str__(self):
+        return f"x: {self.x} y: {self.y} angle: {degrees(self.angle)} ul: {degrees(self.ul)} ur: {degrees(self.ur)} cost: {self.cost}"
+
+class AckermannTrailerDriveState(State):
+
+    # 25 meters per second max speed
+    MAX_V_M = 25
+
+    # Max wheel angle of 45 degrees
+    MAX_PSI_DEG = 45
+
+    WHEEL_DISTANCE_M = 1.75
+
+    DISTANCE_AXLES_M = 5
+
+    WHEELBASE_M = 3
+
+    WHEELBASE_PIXELS = 86
+
+    def __init__(self, x, y, angle, trailer_angle, psi, v, dt):
+        super().__init__(x, y, angle)
+        self.psi = psi
+        self.dt = dt
+        self.v = v
+        self.trailer_angle = trailer_angle
+
+    def get_steps(self, dt, psi_rad, v, current_angle, trailer_current_angle):
+        """ This function returns the steps after integrating the kinematic model over dt """
+        omega = (v/self.WHEELBASE_M)*tan(psi_rad)
+        dangle = omega*dt
+        angle = (current_angle + dangle)%(2*pi)
+
+        dx = v*cos(angle)*dt*self.WHEELBASE_PIXELS/self.WHEELBASE_M
+        dy = -v*sin(angle)*dt*self.WHEELBASE_PIXELS/self.WHEELBASE_M # invert y due to pixel orientation
+
+        trailer_omega = (v/self.DISTANCE_AXLES_M)*sin(angle-trailer_current_angle)
+        trailer_dangle = trailer_omega*dt
+
+        return dangle, trailer_dangle, dx, dy
+
+    def get_neighbors(self, vehicle, obstacles, dt_horizon, open_states, closed_states, goal_state, xlimit, ylimit):
+        """ This function expands the reachable states from the current state given the kinematic constraints
+            and calculates the cost to reach the new states using the path cost and a heuristic cost
+        """
+        neighbors = []
+
+        psi_increment_deg = 5
+        for v in [-self.MAX_V_M, self.MAX_V_M]:
+            for psi_deg in range(-self.MAX_PSI_DEG, self.MAX_PSI_DEG+psi_increment_deg, psi_increment_deg):
+                psi_rad = radians(psi_deg)
+                dangle, trailer_dangle, dx, dy = self.get_steps(dt_horizon, psi_rad, v, self.angle, self.trailer_angle)
+
+                angle = (self.angle + dangle)%(2*pi)
+                trailer_angle = (self.trailer_angle + trailer_dangle)%(2*pi)
+                angle_car_to_trailer = abs(angle - trailer_angle)
+
+                x = round(self.x + dx)
+                y = round(self.y + dy)
+
+                original_x = vehicle.x
+                original_y = vehicle.y
+                original_angle = vehicle.orientation
+                original_trailer_angle = vehicle.trailer_orientation
+
+                # 1. Check if its within map limits and also if the angle makes sense
+                if x >= xlimit[0] and x <= xlimit[1] and y >= ylimit[0] and y <= ylimit[1] and angle_car_to_trailer <= (pi/4.0):
+
+                    # 2. Check if the state hasnt been explored yet
+                    new_state = AckermannTrailerDriveState(x, y, angle, trailer_angle, psi_rad, v, dt_horizon)
+                    if new_state not in closed_states:
+
+                        # 3. Check if the new state is in collision
+                        vehicle.set_orientation(degrees(angle), degrees(trailer_angle))
+                        vehicle.set_position(x, y)
+                        vehicle.update()
+                        colided = vehicle.check_collision(obstacles)
+                        if not colided:
+
+                            # Calculate the new costs
+                            tentative_new_cost = self.cost + self.calculate_euclidean_distance(new_state)
+                            heuristic_cost = new_state.calculate_heuristic_cost(self, goal_state)
+
+                            # Update the state in the open list if it exists
+                            try:
+                                index = open_states.index(new_state)
+                                previous_cost = open_states[index].cost
+                                if tentative_new_cost < previous_cost:
+                                    open_states[index] = new_state
+                                    open_states[index].cost = tentative_new_cost
+                                    open_states[index].total_cost = tentative_new_cost + heuristic_cost
+                                    open_states[index].parent_state = self
+                            except ValueError:
+                                # return a new element in the neighbors list if this hadnt been discovered
+                                new_state.parent_state = self
+                                new_state.cost = tentative_new_cost
+                                new_state.total_cost = tentative_new_cost + heuristic_cost
+                                neighbors.append(new_state)
+                        vehicle.set_orientation(original_angle, original_trailer_angle)
+                        vehicle.set_position(original_x, original_y)
+                        vehicle.update()
+
+        return neighbors
+
+    def calculate_heuristic_cost(self, prev_state, goal_state):
+        """ Heuristic cost calculation penalizing:
+        euclidean distance from goal
+        driving in reverse
+        agressive changes of steering
+        the difference between the current orientation of the vehicle and the goal orientation
+        """
+
+        distance_multiplier = 4
+        reverse_multiplier = 1
+        # if self.v < 0:
+        #     reverse_multiplier = 1.5
+        steering_multiplier = 1
+        # if (self.v * prev_state.v) < 0:
+        #     steering_multiplier = 1.5
+        dist = self.calculate_euclidean_distance(goal_state)
+        angle_diff = degrees(abs(self.angle-goal_state.angle)%(2*pi))
+        trailer_angle_diff = degrees(abs(self.trailer_angle - goal_state.trailer_angle)%(2*pi))
+
+        return steering_multiplier*reverse_multiplier*distance_multiplier*dist# + 2*angle_diff + 2*trailer_angle_diff
+
+    def goal_check(self, other_state):
+        """ Checks if the current state is whithin acceptable tolerance from the goal """
+        dist = self.calculate_euclidean_distance(other_state)
+        angle_diff = degrees(abs(self.angle - other_state.angle)%(2*pi))
+        trailer_angle_diff = degrees(abs(self.trailer_angle - other_state.trailer_angle)%(2*pi))
+
+        return dist <10 and angle_diff < 5 and trailer_angle_diff < 15
+
+    def __eq__(self, other_state):
+        """ Compares 2 states considering certain tolerance """
+        dist = self.calculate_euclidean_distance(other_state)
+        angle_diff = degrees(abs(self.angle - other_state.angle)%(2*pi))
+        trailer_angle_diff = degrees(abs(self.trailer_angle - other_state.trailer_angle)%(2*pi))
+
+        return dist < 30 and angle_diff < 5 and trailer_angle_diff < 5
+
+    def __str__(self):
+        return f"x: {self.x} y: {self.y} angle: {degrees(self.angle)} trailer_angle: {degrees(self.trailer_angle)} psi: {degrees(self.psi)} v: {self.v} cost: {self.cost}"
---
+++ Assignment3/valet_ackerman_trailer.py
@@ -0,0 +1,187 @@
+from pathlib import Path
+import sys
+import pygame
+from math import radians, cos, sin, degrees
+
+from vehicles import TrailerVehicleSprite
+from obstacles import Obstacle
+from states import AckermannTrailerDriveState
+
+SCREEN_WIDTH = 400+800
+SCREEN_HEIGHT = 650
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+FPS = 60
+
+def reconstruct_path(state_chained_list):
+    path = []
+    path.append(state_chained_list.pop())
+    index = 0
+    while path[index].parent_state:
+        path.append(path[index].parent_state)
+        index += 1
+    path.reverse()
+    return path
+
+def search_path(vehicle, obstacles, end_state):
+    max_iterations = 10000
+    dt = 0.09
+    # dt = 0.11
+    # dt = 0.06
+
+    # Actual planning
+    if search_path.open_states and search_path.iterations < max_iterations:
+
+        # Search and remove the state with the lowest cost
+        current_state = search_path.open_states[0]
+        state_index = 0
+        i = 0
+        for state in search_path.open_states:
+            if state.total_cost < current_state.total_cost:
+                current_state = state
+                state_index = i
+            i += 1
+        del search_path.open_states[state_index]
+
+        # Add the current state to the closed list
+        search_path.closed_states.append(current_state)
+
+        if current_state.goal_check(end_state):
+
+            path = reconstruct_path(search_path.closed_states)
+
+            print("success")
+            print("Finished reconstructing path")
+            print("Number of iterations: ", search_path.iterations)
+            print("Number of waypoints: ", len(path))
+            print("Press any key to visualize the path")
+
+            exit_code = 1
+            return exit_code, path, search_path.closed_states
+        else:
+            search_path.iterations += 1
+            neighbors = current_state.get_neighbors(vehicle, obstacles, dt, search_path.open_states, search_path.closed_states, end_state, (0, SCREEN_WIDTH), (0, SCREEN_HEIGHT))
+            search_path.open_states += neighbors
+            exit_code = 0
+            return exit_code, [], search_path.closed_states
+    else:
+        print("Solution not found")
+        exit_code = -1
+        return exit_code, [], search_path.closed_states
+
+def render_all(window, clock, vehicle, obstacles, explored_states, points_in_route):
+    window.fill(WHITE)
+
+    for obstacle in obstacles:
+        obstacle.render(window)
+
+    for state in explored_states:
+        window.fill((0, 0, 0), ((state.x, state.y), (2, 2)))
+
+    if points_in_route:
+
+        point = points_in_route[-1]
+        vehicle.set_orientation(degrees(point[2]), degrees(point[3]))
+        vehicle.set_position(point[0], point[1])
+        vehicle.update()
+        vehicle.render(window)
+
+        for point in points_in_route:
+            window.fill((0, 255, 0), ((point[0],point[1]), (2, 2)))
+
+    pygame.display.update()
+
+    clock.tick(FPS)
+
+def main():
+
+    """ This section is mostly initializing the map and objects in it """
+    project_root = Path(sys.path[0])
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Motion Planning With Kinematic Constraints")
+
+    clock = pygame.time.Clock()
+
+    obstacles = []
+    obstacles.append(Obstacle((400+450,250), 400, 400, RED))
+    obstacles.append(Obstacle((400+100,610), 200, 80, RED))
+    obstacles.append(Obstacle((400+SCREEN_WIDTH/2,SCREEN_HEIGHT+1), SCREEN_WIDTH, 2, RED))
+    obstacles.append(Obstacle((400+SCREEN_WIDTH/2,-1), SCREEN_WIDTH, 2, RED))
+
+    car = TrailerVehicleSprite(str(project_root) + "/assets/tesla.png", str(project_root) + "/assets/tesla.png", 180, 180, (100, 100))
+
+    """ Declaring initial and final tates"""
+
+    initial_state = AckermannTrailerDriveState(100, 100, radians(0), radians(0), 0, 0, 0)
+
+    # Multiple goals for testing
+    # goal_state = AckermannTrailerDriveState(100+150, 400, radians(-90), radians(-90), 0, 0, 0)
+    goal_state = AckermannTrailerDriveState(400+600, 600, radians(0), radians(0), 0, 0, 0)
+    # goal_state = AckermannTrailerDriveState(400+720, 220, radians(90), radians(90), 0, 0, 0)
+
+    """ Variables used for the search algorithm """
+    search_path.open_states = [initial_state]
+    search_path.closed_states = []
+    search_path.iterations = 0
+
+    """ Variables used for flow of the simulation"""
+    path = []
+    path_ready = 0
+    stop = 0
+    start_visualizing = 0
+
+    """ Main loop with search and visualization """
+    run = True
+    while run:
+
+        # Handle user events
+        for event in pygame.event.get():
+            # User clicked on close button
+            if event.type == pygame.QUIT:
+                run = False
+
+            if path_ready and event.type == pygame.KEYDOWN:
+                start_visualizing = 1
+
+        """ A* search """
+        if path_ready == 0 and not stop:
+            path_ready, path, explored_states = search_path(car, obstacles, goal_state)
+
+        """ Path visualization """
+        points_in_route = []
+        if path_ready == 1:
+            if start_visualizing:
+                for i in range(1, len(path)):
+                    time_step = 0.002
+                    current_time = 0
+
+                    prev_state = path[i-1]
+                    state = path[i]
+                    while current_time < state.dt:
+                        current_time += time_step
+                        dangle, trailer_dangle, dx, dy = prev_state.get_steps(current_time, state.psi, state.v, prev_state.angle, prev_state.trailer_angle)
+                        x = prev_state.x + dx
+                        y = prev_state.y + dy
+                        angle = prev_state.angle + dangle
+                        trailer_angle = prev_state.trailer_angle + trailer_dangle
+                        points_in_route.append((x,y,angle, trailer_angle))
+
+                        pygame.event.pump()
+                        render_all(screen, clock, car, obstacles, explored_states, points_in_route)
+
+                path_ready = 0
+                stop = 1
+        elif not stop:
+            render_all(screen, clock, car, obstacles, explored_states, [])
+
+    # Finish execution
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
