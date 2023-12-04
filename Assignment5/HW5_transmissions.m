a0a7d4ed791703fb1446a6157cc50ac7240b4779
davidoises <davidoises@hotmail.com>
2023-12-04 03:08:44 -0800

    Removed commented sections that were needed

--- Assignment5/HW5_transmissions.m
+++ Assignment5/HW5_transmissions.m
@@ -1,6 +1,6 @@
-% close all
-% clear
-% clc
+close all
+clear
+clc
 
 global THICKNESS LENGTH HEIGHT WIDTH
 THICKNESS = 0.025;
@@ -41,17 +41,17 @@ planner = plannerBiRRT(ss,sv,MaxConnectionDistance=0.05,MaxIterations=700);
 start_state = [0.185 0 0.522 rotm2quat(roty(90))];
 goal_state = [0.185 0 0.9 rotm2quat(roty(90))];
 
-% [pthObj,solnInfo] = plan(planner, start_state, goal_state);
+[pthObj,solnInfo] = plan(planner, start_state, goal_state);
 
 solnInfo
 
-% if solnInfo.IsPathFound
-    number_of_nodes_in_solution = size(pthObj.States, 1);
-    states_array = pthObj.States;
-% else
-%     number_of_nodes_in_solution = size(solnInfo.GoalTreeData,1);
-%     states_array = solnInfo.GoalTreeData;
-% end
+if solnInfo.IsPathFound
+   number_of_nodes_in_solution = size(pthObj.States, 1)
+   states_array = pthObj.States;
+else
+    number_of_nodes_in_solution = size(solnInfo.GoalTreeData,1);
+    states_array = solnInfo.GoalTreeData;
+end
 
 figure
 gif('path_solution.gif');
@@ -61,7 +61,7 @@ gif('path_solution.gif');
 for i=1:number_of_nodes_in_solution
     state = states_array(i,:);
 
-    
+
     display_pose(primary_shaft, state);
     xlim([-0.3, 1])
     ylim([-0.5, 0.5])
@@ -71,11 +71,11 @@ for i=1:number_of_nodes_in_solution
     zlabel("z axis")
     view([10 20.75])
     hold on;
-   
+
     draw_obstacles(obstacles,colors);
-    
+
     pause(0.10);
-    
+
     drawnow;
     gif
     hold off
@@ -114,7 +114,7 @@ function [shaft, jntBase] = create_shaft()
     % Initialize robot
     shaft = rigidBodyTree("DataFormat", "column");
     base = shaft.Base;
-    
+
     % Create rigid bodies
     c1 = rigidBody("c1");
     c2 = rigidBody("c2");
@@ -127,7 +127,7 @@ function [shaft, jntBase] = create_shaft()
     c9 = rigidBody("c9");
     cA = rigidBody("cA");
     cB = rigidBody("cB");
-    
+
     % Define joints
     jntBase = rigidBodyJoint("base_joint","fixed");
     jnt1 = rigidBodyJoint("jnt1t2","fixed");
@@ -140,7 +140,7 @@ function [shaft, jntBase] = create_shaft()
     jnt8 = rigidBodyJoint("jnt8t9","fixed");
     jnt9 = rigidBodyJoint("jnt9tA","fixed");
     jntA = rigidBodyJoint("jntAtB","fixed");
-    
+
     % Definition of collision geometries
     cylinders = {struct("radius",0.072/2,"length",0.04,"origin",[0.04/2; 0; 0]),
                  struct("radius",0.18/2,"length",0.016,"origin",[0.016/2; 0; 0]),
@@ -153,39 +153,39 @@ function [shaft, jntBase] = create_shaft()
                  struct("radius",0.18/2,"length",0.016,"origin",[0.016/2; 0; 0]),
                  struct("radius",0.260/2,"length",0.05,"origin",[0.05/2; 0; 0]),
                  struct("radius",0.072/2,"length",0.236,"origin",[0.236/2; 0; 0]),};
-    
+
     bodies = {base, c1, c2, c3, c4, c5, c6, c7, c8, c9, cA, cB};
     joints = {[], jntBase, jnt1, jnt2, jnt3, jnt4, jnt5, jnt6, jnt7, jnt8, jnt9, jntA};
-    
+
     % bodies = {base, c1, c2, c3};
     % joints = {[], jntBase, jnt1, jnt2};
-    
+
     % The pose of the base controls the movement of the whole shaft
     pose = [roty(90) [0.185; 0; 0.522]; 0 0 0 1];
     setFixedTransform(jntBase,pose);
-    
+
     % Attach collision cylinders and set transforms between cylinders
     cylinders_number = length(cylinders);
     for i = 1:cylinders_number
         cylinder_properties = cylinders{i};
-    
+
         % Create collision geometry
         cylinder = collisionCylinder(cylinder_properties.radius, cylinder_properties.length);
         % pose = [eye(3) cylinder_properties.origin; 0 0 0 1];
         pose = [eye(3) [0;0;0]; 0 0 0 1];
         cylinder.Pose = pose;
-    
-        % Set the offset of the cylinder position 
+
+        % Set the offset of the cylinder position
         if i >1
-            setFixedTransform(joints{i+1},trvec2tform([0 0 cylinders{i-1}.origin(1)+cylinder_properties.origin(1)]));    
+            setFixedTransform(joints{i+1},trvec2tform([0 0 cylinders{i-1}.origin(1)+cylinder_properties.origin(1)]));
         end
-    
+
         % Attach the collision geometry to the part of the robot
         addCollision(bodies{i+1}, cylinder)
-    
+
         % Attach the jpoin to the part of the robot
         bodies{i+1}.Joint = joints{i+1};
-    
+
         % Attach the part to the robot itself
         addBody(shaft,bodies{i+1},bodies{i}.Name)
     end
@@ -202,23 +202,23 @@ function walls = create_walls()
              struct("width",(WIDTH-0.16)/2,"height",0.16,"origin",[THICKNESS/2 + LENGTH + THICKNESS/2; 0.16/2 + (WIDTH-0.16)/4; 0.400 + 0.16/2]),
              struct("width",(WIDTH-0.16)/2,"height",0.16,"origin",[THICKNESS/2 + LENGTH + THICKNESS/2; -0.16/2 - (WIDTH-0.16)/4; 0.400 + 0.16/2]),
              struct("width",WIDTH,"height",0.09,"origin",[THICKNESS/2 + LENGTH + THICKNESS/2; 0; 0.400 + 0.16 + 0.09/2]),
-             
+
              % Wall on the left
              struct("width",WIDTH,"height",0.400,"origin",[THICKNESS/2;0;0.400/2]),
              struct("width",(WIDTH-0.16)/2,"height",0.16,"origin",[THICKNESS/2; 0.16/2 + (WIDTH-0.16)/4; 0.400 + 0.16/2]),
              struct("width",(WIDTH-0.16)/2,"height",0.16,"origin",[THICKNESS/2; -0.16/2 - (WIDTH-0.16)/4; 0.400 + 0.16/2]),
              struct("width",WIDTH,"height",0.09,"origin",[THICKNESS/2; 0; 0.400 + 0.16 + 0.09/2]),
             };
-    
+
     walls = {};
     boxes_number = length(boxes);
     for i = 1:boxes_number
         box_properties = boxes{i};
-    
+
         box = collisionBox(THICKNESS, box_properties.width, box_properties.height);
         pose = [eye(3) box_properties.origin; 0 0 0 1];
         box.Pose = pose;
-    
+
         walls{i} = box;
     end
 end
@@ -236,16 +236,16 @@ function secondary_shaft = create_secondary_shaft()
                  struct("radius",0.072/2,"length",0.182,"origin",[THICKNESS/2 + 0.051 + 0.05 + 0.076 + 0.05 + 0.016 + 0.05 + 0.05 + 0.182/2;0;0.25]),
                  struct("radius",0.280/2,"length",0.05,"origin",[THICKNESS/2 + 0.051 + 0.05 + 0.076 + 0.05 + 0.016 + 0.05 + 0.05 + 0.182 + 0.05/2;0;0.25]),
                  struct("radius",0.072/2,"length",0.033,"origin",[THICKNESS/2 + 0.051 + 0.05 + 0.076 + 0.05 + 0.016 + 0.05 + 0.05 + 0.182 + 0.05 + 0.033/2;0;0.25])};
-    
+
     secondary_shaft = {};
     cylinders_number = length(cylinders);
     for i = 1:cylinders_number
         cylinder_properties = cylinders{i};
-    
+
         cylinder = collisionCylinder(cylinder_properties.radius, cylinder_properties.length);
         pose = [roty(90) cylinder_properties.origin; 0 0 0 1];
         cylinder.Pose = pose;
-    
+
         % secondary_shaft = [secondary_shaft, cylinder];
         secondary_shaft{i} = cylinder;
     end
